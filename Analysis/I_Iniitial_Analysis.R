94d3e6e51f3f60d394fca42094f9d255435bb079
Nate Jones <njones@sesync.org>
2019-06-29 17:11:40 -0400

    Subshed Delineation

--- Analysis/I_Iniitial_Analysis.R
+++ Analysis/I_Iniitial_Analysis.R
@@ -13,6 +13,7 @@ remove(list=ls())
 
 #download required packages
 library(foreign)
+library(fasterize)
 library(stars)
 library(sf)
 library(raster)
@@ -109,21 +110,22 @@ system(paste(paste(wbt_dir),
 fac<-raster(paste0(scratch_dir,"fac.tif"))
 fdr<-raster(paste0(scratch_dir,"fdr.tif"))
 
-#3.0 Delineate entire upstream watershe for each pond---------------------------
+#3.0 Delineate entire upstream watershed for each pond--------------------------
 #Create folder to export watershed shapefile too
 dir.create(paste0(data_dir, 'modified_data/watersheds'))
 
 #Create function for delineation
 fun<-function(n){
 
+  #Step 1: Delineate watershed with low res DEM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   #Define pond id
   id<-ponds$pond_id[n]
   
   #isolate pond
   pond<-ponds %>% filter(pond_id==id)
   
-  #Add 10 m buffer (10 m comes from the resolutin of the raster)
-  pond<-st_buffer(pond, 10)
+  #Add 3 m buffer (10 m comes from the resolutin of the raster)
+  pond<-st_buffer(pond, 10/3)
   
   #Find max fac point within pond
   fac_pond<-crop(fac, as_Spatial(pond))
@@ -156,13 +158,121 @@ fun<-function(n){
   #Convert watershed to polygon
   w_shp<- w_grd %>% st_as_stars() %>% st_as_sf(., merge = TRUE)
   
+  #Step 2: Re-delineate watershed with high res DEM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  #Clip high resolution DEM with w_shp
+  mask<-st_buffer(w_shp, 100)
+  dem_hr<-crop(dem_1m, as_Spatial(mask))
+
+  #Write raster to scratch workspace
+  writeRaster(dem_hr,paste0(scratch_dir,"dem_hr.tif"), overwrite=T)
+  
+  #Breach Analysis of the DEM
+  system(paste(paste(wbt_dir),
+               "-r=BreachDepressions",
+               paste0("--wd=",scratch_dir),
+               "--dem=dem_hr.tif",
+               "-o=dem_breach_hr.tif"))
+  
+  #Flow Direction
+  system(paste(paste(wbt_dir), 
+               "-r=D8Pointer", 
+               paste0("--wd=",scratch_dir),
+               "--dem='dem_breach_hr.tif'", 
+               "-o='fdr_hr.tif'",
+               "--out_type=sca"))
+  
+  #Flow Accumulation
+  system(paste(paste(wbt_dir), 
+               "-r=DInfFlowAccumulation", 
+               paste0("--wd=",scratch_dir),
+               "--dem='dem_breach_hr.tif'", 
+               "-o='fac_hr.tif'",
+               "--out_type=sca"))
+  
+  #Read fac and fdr into R environment
+  fac_hr<-raster(paste0(scratch_dir,"fac_hr.tif"))
+  fdr_hr<-raster(paste0(scratch_dir,"fdr_hr.tif"))
+  
+  #Find max fac point within pond
+  fac_pond_hr<-crop(fac_hr, as_Spatial(pond))
+  fac_pond_hr<-mask(fac_pond_hr, as_Spatial(pond))
+  
+  #create pour point
+  pp_hr<-rasterToPoints(fac_pond_hr) %>% 
+    #convert to tibble
+    as_tibble() %>%
+    #select point with max fac
+    filter(fac_hr==max(fac_hr))
+  
+  #Make pour point an sf shape
+  pp_hr<-st_as_sf(pp_hr, coords = c("x", "y"), crs = st_crs(pond))
+  
+  #Export pour point to scratch workspace
+  write_sf(pp_hr, paste0(scratch_dir,"pp_hr.shp"), delete_layer = T)
+  
+  #Delineate Watershed w/ WBT
+  system(paste(paste(wbt_dir), 
+               "-r=Watershed", 
+               paste0("--wd=",scratch_dir),
+               "--d8_pntr='fdr_hr.tif''", 
+               "--pour_pts='pp_hr.shp'",
+               "-o='watershed_hr.tif"))
+  
+  #Read watershed in
+  w_hr_grd<-raster(paste0(scratch_dir,"watershed_hr.tif"))
+  
+  #Convert watershed to polygon
+  w_hr_shp<- w_hr_grd %>% st_as_stars() %>% st_as_sf(., merge = TRUE)
+  
   #Add id info and write to output folder
-  w_shp$pond_id<-id
-  write_sf(w_shp, paste0(data_dir, "modified_data/watersheds/",id,".shp"))
+  w_hr_shp$pond_id<-id
+  write_sf(w_hr_shp, paste0(data_dir, "modified_data/watersheds/",id,".shp"), delete_layer = T)
 }
 
 #Apply function (note, because you are using WBT, this has to be single threaded)
-lapply(fun, seq(1,nrow(ponds)))
+lapply(seq(1,nrow(ponds)),fun)
+
+#4.0 Delineate pond subshed-----------------------------------------------------
+#Create folder to export subshed shapefile too
+dir.create(paste0(data_dir, 'modified_data/subsheds'))
+
+#Create function to identify subsheds
+fun<-function(n){
+
+  #Define pond id
+  id<-ponds$pond_id[n]
+  
+  #isolate pond
+  pond<-ponds %>% filter(pond_id==id)
+  
+  #load pond watershed
+  ws_shp<-st_read(paste0(data_dir, "modified_data/watersheds/",id,".shp"))
+
+  #Determine if there are any upstream ponds
+  upstream_ponds<-ponds[ws_shp,] %>% filter(pond_id!=id)
+
+  #If there are upstream ponds, clip them out
+  if(nrow(upstream_ponds)>0){
+    #Create loop to crop polygons that are completely contained by upstream watershed
+    for(i in 1:nrow(upstream_ponds)){
+      upstream_subshed<-st_read(paste0(data_dir, "modified_data/watersheds/",upstream_ponds$pond_id[i],".shp"))
+      if(nrow(upstream_subshed[pond,])==0){
+        ws_shp<-st_difference(ws_shp, upstream_subshed)
+      }
+    }
+  }
+  
+  #Export subshed shape
+  ws_shp$pond_id<-id
+  write_sf(ws_shp, paste0(data_dir, "modified_data/subsheds/",id,".shp"), delete_layer = T)
+}
+  
+#Apply function 
+lapply(seq(1,nrow(ponds)),fun)
+
+
+#5.0 Estimate Pond Hydrogeomorphic Characteristics------------------------------
 
 
 
